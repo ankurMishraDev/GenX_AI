00b2fd78e7a30c4de3faad4c579410e963e9f3ce
Ankur <am8468616@gmail.com>
2025-06-23 10:50:34 +0530

    navbar design and home page UI setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,63 @@
 
 import React from 'react'
 
-const HomePage = () => {
+const HomePage =async () => {
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      
+    <div className="flex flex-col min-h-screen text-foreground overflow-hidden">
+      <section className="relative z-10 py-24 flex-grow">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative">
+            {/* CORNER DECARATION */}
+            <div className="absolute -top-10 left-0 w-40 h-40 border-l-2 border-t-2" />
+
+            {/* LEFT SIDE CONTENT */}
+            <div className="lg:col-span-7 space-y-8 relative">
+              <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight">
+                <div>
+                  <span className="text-foreground">Transform</span>
+                </div>
+                <div>
+                  <span className="text-primary">Your Body</span>
+                </div>
+                <div className="pt-2">
+                  <span className="text-foreground">With Advanced</span>
+                </div>
+                <div className="pt-2">
+                  <span className="text-foreground">AI</span>
+                  <span className="text-primary"> Technology</span>
+                </div>
+              </h1>
+               {/* SEPERATOR LINE */}
+              <div className="h-px w-full bg-gradient-to-r from-primary via-secondary to-primary opacity-50"></div>
+
+              <p className="text-xl text-muted-foreground w-2/3">
+                Talk to our AI assistant and get personalized diet plans and workout routines
+                designed just for you
+              </p>
+
+              {/* STATS */}
+              <div className="flex items-center gap-10 py-6 font-mono">
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">100+</div>
+                  <div className="text-xs uppercase tracking-wider">ACTIVE USERS</div>
+                </div>
+                <div className="h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent"></div>
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">3min</div>
+                  <div className="text-xs uppercase tracking-wider">GENERATION</div>
+                </div>
+                <div className="h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent"></div>
+                <div className="flex flex-col">
+                  <div className="text-2xl text-primary">100%</div>
+                  <div className="text-xs uppercase tracking-wider">PERSONALIZED</div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        
+      </section>
     </div>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+"use client"
+import Link from "next/link";
+import {SignInButton, SignUpButton, UserButton, useUser} from "@clerk/nextjs";
+import {HomeIcon, NotebookIcon, User2Icon, ZapIcon} from "lucide-react";
+import {Button} from "@/components/ui/button";
+const Navbar = () => {
+    const {isSignedIn} = useUser();
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md border-b border-border py-3">
+        <div className="container mx-auto flex items-center justify-between">
+        {/*Logo part */}
+            <Link className="flex items-center gap-2" href="/">
+            <div className="p-1 bg-primary/10 rounded">
+            <ZapIcon className="w-4 h-4 text-primary"/>
+            </div>
+            <span className="text-xl font-bold font-mono">GenX_AI</span>
+            </Link>
+
+        {/*Navigation part */}
+        <nav className="flex items-center gap-5">
+            {isSignedIn ? (
+                <>
+                <Link href="/" className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors">
+                <HomeIcon size={16} />
+                <span>Home</span>
+                </Link>
+
+                <Link href="/generate-program" className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors">
+                <NotebookIcon size={16} />
+                <span>Program</span>
+                </Link>
+
+                <Link href="/profile" className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors">
+                <User2Icon size={16} />
+                <span>Profile</span>
+                </Link>
+
+                <Button asChild variant="outline" className="ml-2 border-primary/65 text-primary hover:text-white hover:bg-primary/10">
+                <Link href="/generate-program">Proceed</Link>
+                </Button>
+                <UserButton/>
+                </>
+            ):(
+                <>
+               <SignInButton>
+                <Button variant={"outline"} className="border-primary/50 text-primary hover:text-white hover:bg-primary/10">
+                Login
+                </Button>
+               </SignInButton>
+
+               <SignUpButton>
+                <Button className="bg-primary text-primary-foreground hover:bg-primary/90">Sign Up</Button>
+               </SignUpButton>
+               </>
+            )}
+        </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
