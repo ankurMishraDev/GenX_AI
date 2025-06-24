bf52284ecb41c08e69b8b25d94ce38f6498615ca
Ankur <am8468616@gmail.com>
2025-06-24 15:53:38 +0530

    sample user details update on home page with footer design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,17 @@ import { ArrowRightIcon } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import SampleProgramDisplay from '@/components/SampleProgramDisplay'
 
 const HomePage =async () => {
 
   return (
     <div className="flex flex-col min-h-screen text-foreground overflow-hidden">
-      <section className="relative z-10 py-24 flex-grow">
+      <section className="relative z-10 py-5 flex-grow">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative">
             {/* CORNER DECARATION */}
-            <div className="absolute -top-10 left-0 w-40 h-40 border-l-2 border-t-2" />
+            <div className="absolute -top-5 left-0 w-40 h-40 border-l-2 border-t-2" />
 
             {/* LEFT SIDE CONTENT */}
             <div className="lg:col-span-7 space-y-8 relative">
@@ -109,6 +110,7 @@ const HomePage =async () => {
           </div>
         
       </section>
+      <SampleProgramDisplay />
     </div>
   )
 }
